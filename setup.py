fc4c9297b7793fb8c2b90734c6c61f7099565f7f
Devesh Agrawal <dagrawal@uber.com>
2019-02-12 11:16:57 -0800

    Bumping version again for the description etc

--- setup.py
+++ setup.py
@@ -53,8 +53,7 @@ with open(os.path.join(here, NAME, '__version__.py')) as f:
 from setuptools import setup
 
 # read the contents of your README file
-this_directory = os.path.abspath(path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 class UploadCommand(Command):
@@ -97,7 +96,7 @@ setup(
     version=about['__version__'],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
