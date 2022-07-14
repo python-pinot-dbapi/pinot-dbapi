6f3cb0a33db7a155be40d14ea945dbdcc4570aae
egalpin <egalpin@users.noreply.github.com>
2022-07-14 13:34:18 -0400

    Updates example to show parallel requests with gather

--- examples/pinot-async.py
+++ examples/pinot-async.py
@@ -1,49 +1,49 @@
 import asyncio
+import time
 
 import httpx
-from pinotdb import connect
+from pinotdb import connect_async
 
 
-async def foo():
-    async with connect(host='localhost', port=8099, path='/query/sql',
-                       scheme='https', verify_ssl=False, use_async=True) as conn:
-        curs = await conn.execute_async("""
+async def show_async_features():
+    async with connect_async(host='localhost', port=8099, path='/query/sql',
+                       scheme='http', verify_ssl=False) as conn:
+        curs = await conn.execute("""
             SELECT count(*)
-              FROM foo
+              FROM calls
              LIMIT 10
         """)
         for row in curs:
             print(row)
 
-    conn = connect(
-        host='localhost', port=8099, path='/query/sql', scheme='https',
-        verify_ssl=False, use_async=True)
-
-    curs = conn.cursor()
-    await curs.execute_async("""
-        SELECT count(*)
-          FROM foo
-         LIMIT 10
-    """)
-    for row in curs:
-        print(row)
-
-    # Externally managed client session
+    # Externally managed client session can also be passed to connect_async
     session = httpx.AsyncClient(verify=False)
-    conn = connect(
-        host='localhost', port=8099, path='/query/sql', scheme='https',
-        verify_ssl=False, use_async=True, session=session)
+    conn = connect_async(
+        host='localhost', port=8099, path='/query/sql', scheme='http',
+        verify_ssl=False, session=session)
 
-    await curs.execute_async("""
-        SELECT count(*)
-          FROM foo
-         LIMIT 10
-    """)
-    for row in curs:
-        print(row)
+    # launch 10 requests in parallel spanning a limit/offset range
+    reqs = []
+    step = 10
+    num_requests = 10
+    start = time.perf_counter()
+    for i in range(num_requests):
+        req = conn.execute(f"""
+              SELECT count(*)
+              FROM calls
+              LIMIT 10, {i * step}
+        """)
+        reqs.append(req)
+
+    for curs in await asyncio.gather(*reqs):
+        for row in curs:
+            print(row)
+
+    print(f'{num_requests} requests took {time.perf_counter() - start} '
+          'seconds')
 
     # close all cursors
-    await conn.close_async()
+    await conn.close()
 
     # don't forget to close the session in the case where it was provided to
     # connect
