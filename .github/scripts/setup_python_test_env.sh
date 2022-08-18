249bc7db873fa23eb863d9f4cc0891b43a634da5
Rong Rong <rongr@apache.org>
2022-08-18 09:49:13 -0700

    Make CI green! (#39)
    * fix docker run config (args, permission, detach mode)
    * adding wait for cluster setup, adding python env setup.
    * adding unit-test with examples
    * stop container using if always()
    
    Co-authored-by: Rong Rong <rongr@startree.ai>

--- .github/scripts/setup_python_test_env.sh
+++ .github/scripts/setup_python_test_env.sh
@@ -1,1 +1,42 @@
-#/bin/bash
+#!/bin/bash
+
+
+
+### ---------------------------------------------------------------------------
+### Install python packages
+### ---------------------------------------------------------------------------
+
+echo "Install Python Packages"
+
+pip install -r ./requirements.txt
+pip install -U urllib3
+
+
+### ---------------------------------------------------------------------------
+### Ensure Pinot cluster started correctly.
+### ---------------------------------------------------------------------------
+
+echo "Ensure Pinot cluster started correctly"
+
+# Wait at most 5 minutes to reach the desired state
+PASS=0
+
+for i in $(seq 1 150)
+do
+  QUERY_RES=`curl -X POST --header 'Accept: application/json' -d '{"sql":"select count(*) from baseballStats limit 1"}' http://localhost:${BROKER_PORT_FORWARD}/query/sql`
+  if [ $? -eq 0 ]; then
+    COUNT_STAR_RES=`echo "${QUERY_RES}" | jq '.resultTable.rows[0][0]'`
+    if [[ "${COUNT_STAR_RES}" =~ ^[0-9]+$ ]] && [ "${COUNT_STAR_RES}" -gt 0 ]; then
+      PASS=1
+      break
+    fi
+  fi
+  echo "QUERY_RES: ${QUERY_RES}"
+  sleep 2
+done
+
+if [ "${PASS}" -eq 0 ]; then
+  echo 'Quickstart failed: Cannot confirmed count-star result from quickstart table in 5 minutes'
+  exit 1
+fi
+echo "Pinot cluster started correctly"
--- tests/test_wrap_example.py
+++ tests/test_wrap_example.py
@@ -6,11 +6,13 @@ import io
 from contextlib import redirect_stdout, redirect_stderr
 from typing import *
 
-from .testing_context import TestCase
+from tests.testing_context import TestCase
 
 
 class ExampleWrapper(TestCase):
 
+    # TODO: add rest of the example in
+    # TODO: break each example into individual TestCase subclass
     test_examples: List[str] = ["pinot_quickstart_auth_zk", "pinot_quickstart_batch", "pinot_async"]
 
     # TODO: break this into multiple instantiable TestCase sub classes, one for each example file
