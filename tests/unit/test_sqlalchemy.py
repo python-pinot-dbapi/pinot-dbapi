453c346f18a9378ee675baef1462b607e978edde
Diogo Baeder <diogobaeder@yahoo.com.br>
2023-02-27 00:04:05 -0300

    Covering all compilations in PinotTypeCompiler.

--- tests/unit/test_sqlalchemy.py
+++ tests/unit/test_sqlalchemy.py
@@ -12,10 +12,10 @@ for now.
 
 from unittest import TestCase
 
-from sqlalchemy import Column, Integer, MetaData, Table, column, select
+from sqlalchemy import Column, Integer, MetaData, REAL, Table, column, select
 from sqlalchemy.engine import make_url
 
-from pinotdb import sqlalchemy as ps
+from pinotdb import exceptions, sqlalchemy as ps
 
 
 class PinotTestCase(TestCase):
@@ -89,3 +89,77 @@ class PinotCompilerTest(PinotTestCase):
             str(compiler),
             'SELECT "order" AS ord \nFROM some_table',
         )
+
+
+class PinotTypeCompilerTest(PinotTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.compiler: ps.PinotTypeCompiler = self.dialect.type_compiler
+
+    def test_compiles_real(self):
+        self.assertEqual(self.compiler.visit_REAL(None), 'DOUBLE')
+
+    def test_compiles_numeric(self):
+        self.assertEqual(self.compiler.visit_NUMERIC(None), 'LONG')
+
+    def test_compiles_decimal(self):
+        self.assertEqual(self.compiler.visit_DECIMAL(None), 'LONG')
+
+    def test_compiles_integer(self):
+        self.assertEqual(self.compiler.visit_INTEGER(None), 'LONG')
+
+    def test_compiles_smallint(self):
+        self.assertEqual(self.compiler.visit_SMALLINT(None), 'LONG')
+
+    def test_compiles_bigint(self):
+        self.assertEqual(self.compiler.visit_BIGINT(None), 'LONG')
+
+    # TODO: Check if this is correct (seems strange to have boolean as long).
+    def test_compiles_boolean(self):
+        self.assertEqual(self.compiler.visit_BOOLEAN(None), 'LONG')
+
+    def test_compiles_timestamp(self):
+        self.assertEqual(self.compiler.visit_TIMESTAMP(None), 'LONG')
+
+    def test_compiles_date(self):
+        self.assertEqual(self.compiler.visit_DATE(None), 'LONG')
+
+    def test_compiles_char(self):
+        self.assertEqual(self.compiler.visit_CHAR(None), 'STRING')
+
+    def test_compiles_nchar(self):
+        self.assertEqual(self.compiler.visit_NCHAR(None), 'STRING')
+
+    def test_compiles_varchar(self):
+        self.assertEqual(self.compiler.visit_VARCHAR(None), 'STRING')
+
+    def test_compiles_nvarchar(self):
+        self.assertEqual(self.compiler.visit_NVARCHAR(None), 'STRING')
+
+    def test_compiles_text(self):
+        self.assertEqual(self.compiler.visit_TEXT(None), 'STRING')
+
+    def test_compiles_binary(self):
+        self.assertEqual(self.compiler.visit_BINARY(None), 'BYTES')
+
+    def test_compiles_varbinary(self):
+        self.assertEqual(self.compiler.visit_VARBINARY(None), 'BYTES')
+
+    def test_compiles_datetime(self):
+        self.assertEqual(self.compiler.visit_DATETIME(None), 'TIMESTAMP')
+
+    def test_cannot_compile_time(self):
+        with self.assertRaises(exceptions.NotSupportedError):
+            self.compiler.visit_TIME(None)
+
+    def test_cannot_compile_blob(self):
+        with self.assertRaises(exceptions.NotSupportedError):
+            self.compiler.visit_BLOB(None)
+
+    def test_cannot_compile_clob(self):
+        with self.assertRaises(exceptions.NotSupportedError):
+            self.compiler.visit_CLOB(None)
+
+    def test_cannot_compile_nclob(self):
+        with self.assertRaises(exceptions.NotSupportedError):
+            self.compiler.visit_NCLOB(None)
