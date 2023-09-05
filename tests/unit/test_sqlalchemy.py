42df0fdd2c1b212cd3365d4d2f45536d57985a4b
Diogo Baeder <diogobaeder@yahoo.com.br>
2023-09-05 12:54:47 -0300

    Finishing coverage (#75)

--- tests/unit/test_sqlalchemy.py
+++ tests/unit/test_sqlalchemy.py
@@ -13,7 +13,10 @@ for now.
 from unittest import TestCase
 
 import responses
-from sqlalchemy import Column, Integer, MetaData, Table, column, select
+from sqlalchemy import (
+    BigInteger, Column, Integer, MetaData, String, Table,
+    column, select,
+)
 from sqlalchemy.engine import make_url
 
 import pinotdb
@@ -92,6 +95,194 @@ class PinotDialectTest(PinotTestCase):
 
         self.assertEqual(metadata, {'foo': 'bar'})
 
+    @responses.activate
+    def test_cannot_get_metadata_if_broken_json(self):
+        url = f'{self.dialect._controller}/some-path'
+        responses.get(url, body='something')
+
+        with self.assertRaises(exceptions.DatabaseError):
+            self.dialect.get_metadata_from_controller('some-path')
+
+    def test_gets_schema_names(self):
+        names = self.dialect.get_schema_names('some connection')
+
+        self.assertEqual(names, ['default'])
+
+    @responses.activate
+    def test_gets_table_names_from_controller(self):
+        url = f'{self.dialect._controller}/tables'
+        responses.get(url, json={'tables': ['foo', 'bar']})
+
+        names = self.dialect.get_table_names('some connection')
+
+        self.assertEqual(names, ['foo', 'bar'])
+
+    @responses.activate
+    def test_checks_that_table_exists(self):
+        url = f'{self.dialect._controller}/tables'
+        responses.get(url, json={'tables': ['foo', 'bar']})
+
+        self.assertTrue(self.dialect.has_table('some connection', 'foo'))
+        self.assertFalse(self.dialect.has_table('some connection', 'none'))
+
+    def test_gets_empty_views(self):
+        self.assertEqual(self.dialect.get_view_names('conn'), [])
+
+    def test_gets_empty_table_options(self):
+        self.assertEqual(self.dialect.get_table_options('conn', 'table'), {})
+
+    @responses.activate
+    def test_gets_columns_from_server(self):
+        table_name = 'some-table'
+        url = f'{self.dialect._controller}/tables/{table_name}/schema'
+        responses.get(url, json={
+            'tables': [table_name],
+            'timeFieldSpec': {},
+            'dimensionFieldSpecs': [{'name': 'foo', 'dataType': 'STRING'}],
+            'metricFieldSpecs': [{'name': 'bar', 'dataType': 'STRING'}],
+            'dateTimeFieldSpecs': [{'name': 'baz', 'dataType': 'STRING'}],
+        })
+
+        columns = self.dialect.get_columns('conn', table_name)
+
+        self.assertEqual(columns, [
+            {
+                'default': None,
+                'name': 'foo',
+                'nullable': True,
+                'type': String,
+            },
+            {
+                'default': None,
+                'name': 'bar',
+                'nullable': True,
+                'type': String,
+            },
+            {
+                'default': None,
+                'name': 'baz',
+                'nullable': True,
+                'type': String,
+            },
+        ])
+
+    @responses.activate
+    def test_gets_columns_with_different_default_values(self):
+        table_name = 'some-table'
+        url = f'{self.dialect._controller}/tables/{table_name}/schema'
+        responses.get(url, json={
+            'tables': [table_name],
+            'timeFieldSpec': {},
+            'dimensionFieldSpecs': [{
+                'name': 'foo',
+                'dataType': 'INT',
+                'defaultNullValue': 123,
+            }],
+        })
+
+        columns = self.dialect.get_columns('conn', table_name)
+
+        self.assertEqual(columns, [
+            {
+                'default': '123',
+                'name': 'foo',
+                'nullable': True,
+                'type': BigInteger,
+            },
+        ])
+
+    @responses.activate
+    def test_gets_columns_with_time_spec(self):
+        table_name = 'some-table'
+        url = f'{self.dialect._controller}/tables/{table_name}/schema'
+        responses.get(url, json={
+            'tables': [table_name],
+            'timeFieldSpec': {
+                'incomingGranularitySpec': {
+                    'name': 'time', 'dataType': 'STRING'}
+            },
+            'dateTimeFieldSpecs': [],
+        })
+
+        columns = self.dialect.get_columns('conn', table_name)
+
+        self.assertEqual(columns, [
+            {
+                'default': None,
+                'name': 'time',
+                'nullable': True,
+                'type': String,
+            },
+        ])
+
+    def test_gets_pk_constraint(self):
+        result = self.dialect.get_pk_constraint('conn', 'some-table')
+
+        self.assertEqual(result, {"constrained_columns": [], "name": None})
+
+    def test_gets_table_comment(self):
+        result = self.dialect.get_table_comment('conn', 'some-table')
+
+        self.assertEqual(result, {"text": ""})
+
+    def test_gets_foreign_keys(self):
+        result = self.dialect.get_foreign_keys('conn', 'some-table')
+
+        self.assertEqual(result, [])
+
+    def test_gets_check_constraints(self):
+        result = self.dialect.get_check_constraints('conn', 'some-table')
+
+        self.assertEqual(result, [])
+
+    def test_gets_indexes(self):
+        result = self.dialect.get_indexes('conn', 'some-table')
+
+        self.assertEqual(result, [])
+
+    def test_gets_unique_constraints(self):
+        result = self.dialect.get_unique_constraints('conn', 'some-table')
+
+        self.assertEqual(result, [])
+
+    def test_gets_view_definition(self):
+        self.assertIsNone(self.dialect.get_view_definition('conn', 'table'))
+
+    def test_cannot_rollback(self):
+        self.assertIsNone(self.dialect.do_rollback('conn'))
+
+    def test_checks_unicode_returns(self):
+        self.assertTrue(self.dialect._check_unicode_returns('conn'))
+
+    def test_checks_unicode_description(self):
+        self.assertTrue(self.dialect._check_unicode_description('conn'))
+
+
+class PinotMultiStageDialectTest(PinotTestCase):
+    def setUp(self) -> None:
+        self.dialect = ps.PinotMultiStageDialect(
+            server='http://localhost:9000')
+
+    def test_creates_connection_args(self):
+        url = make_url(
+            'pinot://localhost:8000/query/sql?controller='
+            'http://localhost:9000/')
+
+        cargs, cparams = self.dialect.create_connect_args(url)
+
+        self.assertEqual(cargs, [])
+        self.assertEqual(cparams, {
+            'debug': False,
+            'scheme': 'http',
+            'host': 'localhost',
+            'port': 8000,
+            'path': 'query/sql',
+            'username': None,
+            'password': None,
+            'verify_ssl': True,
+            'use_multistage_engine': True,
+        })
+
 
 class PinotCompilerTest(PinotTestCase):
     def test_can_do_simple_select(self):
@@ -137,6 +328,21 @@ class PinotCompilerTest(PinotTestCase):
         self.assertEqual(
             str(compiler),
             'SELECT "order" AS ord \nFROM some_table',
+        )
+
+    def test_can_select_table_directly(self):
+        metadata = MetaData()
+        table = Table(
+            'some_table', metadata,
+            Column('some_column', Integer)
+        )
+        statement = select(table.c.some_column)
+
+        compiler = self.dialect.statement_compiler(self.dialect, statement)
+
+        self.assertEqual(
+            str(compiler),
+            'SELECT some_column \nFROM some_table',
         )
 
 
