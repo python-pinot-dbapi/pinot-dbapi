0e1d57bf2759c42021ca75d0bc8434e00336cb86
Xiang Fu <xiangfu@startree.ai>
2026-01-02 05:29:56 -0800

    Fix examples for SQLAlchemy 2.0 compatibility

--- examples/pinot_quickstart_multi_stage.py
+++ examples/pinot_quickstart_multi_stage.py
@@ -50,7 +50,7 @@ def run_pinot_quickstart_multi_stage_sqlalchemy_example() -> None:
 def run_pinot_quickstart_multi_stage_sqlalchemy_example_2() -> None:
 
     engine = create_engine(
-        "pinot://localhost:8000/query?controller=http://localhost:9000/",
+        "pinot://localhost:8000/query/sql?controller=http://localhost:9000/",
         connect_args={"useMultistageEngine": "true"}
     )  # uses HTTP by default :(
     # engine = create_engine('pinot+http://localhost:8000/query/sql?controller=http://localhost:9000/')
